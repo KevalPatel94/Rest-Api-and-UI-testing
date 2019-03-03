4a66ff580d07774e1eb93aa39c965efa5d08495c
Patel, Keval Tulsibhai <patelk0863@d150-47078.local>
2019-03-03 13:26:34 -0600

    Initial Commit

---
+++ ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjC
+//
+//  Created by Patel, Keval Tulsibhai on 3/3/19.
+//  Copyright © 2019 Patel, Keval Tulsibhai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
